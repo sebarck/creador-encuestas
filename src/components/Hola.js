914220184c3cf39d8c3a1494aeb8bdb8d9bead46
Cristian <cristian.merenda@gmail.com>
2020-09-09 11:13:25 -0300

    prueba de componentes multiples

---
+++ src/components/Hola.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export class Hola extends Component {
+    render () {
+        return <p>{this.props.mensaje}</p>
+    }
+}
--- src/components/View.js
+++ src/components/View.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react'
 import { SimpleQuestion } from './SimpleQuestion'
 import { ViewSimpleQuestion } from './ViewSimpleQuestion'
+import { Hola } from './Hola'
 
 export class View extends Component {
+
+
+
 
     state = {
         pregunta: ""
     }
+
+
+    componentList = [
+        <Hola  mensaje='probando1'/>,
+        <Hola mensaje='probando2'/>
+    ]; // Change this to get the list from props or state
 
     _addSimpleNewQuestion = (pregunta) => {
         console.log('Agregando pregunta')
         this.setState({pregunta})
     }
 
+    generarHola (){
+        
+        this.componentList.concat({Hola mensaje="otroMensaje" />})
+        
+    }
+
     render () {
         return (
             <div>
+                {this.generarHola()}
+                {this.componentList}
                 <SimpleQuestion addNewQuestion={this._addSimpleNewQuestion} />
                 <ViewSimpleQuestion pregunta={this.state.pregunta} />
             </div>
