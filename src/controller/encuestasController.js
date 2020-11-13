d52f388897f2b4af04c3b0fad99ec319f8c72a83
Cristian <cristian.merenda@gmail.com>
2020-11-13 02:34:24 -0300

    Correccion dde response de fetch alta encuesta

--- src/controller/encuestasController.js
+++ src/controller/encuestasController.js
@@ -21,3 +21,23 @@ exports.generarEncuesta = (body,callbackResponse, callbackData, callbackCatch) =
             .catch(e => callbackCatch(e))    
 }
 
+exports.generarEncuesta = (body,callbackResponse, callbackData, callbackCatch) => {
+    const url='http://localhost:8080/api/v1/encuestas'
+        const myHeaders = new Headers({
+            'Accept':'application/x-www-form-urlencoded, application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': "http://localhost:3000"
+        })
+        const myInit = {
+            method: 'POST',
+            headers: myHeaders,
+            mode: 'cors',
+            cache: 'default',
+            body: JSON.stringify(body)
+        };
+
+        fetch(url,myInit)
+            .then(response => response.json())
+            .then(data => callbackData(data))
+            .catch(e => callbackCatch(e))    
+}
